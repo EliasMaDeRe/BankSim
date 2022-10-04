e6d2589ed52fa7d589e28adc2a3adaab67d468f0
FernandoJoachin <a18000621@alumnos.uady.mx>
2022-10-04 03:49:55 -0500

    Update Cuenta.java
    si

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -3,19 +3,69 @@ import java.util.Date;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
-
+import java.text.DateFormat;
 public class Cuenta {
 
     public static int getSaldo(String idCliente, String idCuenta){
-
-        return 1;
-
+        int contador = 0;
+        int saldo = 0;
+        if(Ruta.existe(idCliente,idCuenta)){
+            File rutaCuenta = new File(Ruta.path(idCliente,idCuenta));
+            try{
+                File cuenta = new File (rutaCuenta.getPath()+"/info.txt");
+                Scanner archivo = new Scanner(cuenta);
+                String info;
+                
+                while(archivo.hasNextLine()){
+                    info = archivo.nextLine();
+                    if(contador == 1){
+                        saldo = Integer.valueOf(info);
+                    }
+                    contador++;
+                }
+                archivo.close();
+            }catch (Exception e) {
+               System.out.println("Ocurrio un error");
+            }
+            return saldo;
+            
+        }else{
+            return -1;
+        }
     }
 
-    public static void putSaldo(String idCliente, String idCuenta){
+    public static void putSaldo(String idCliente, String idCuenta, int saldo){
+        int contador = 0;
+        String arreglo [] = new String [4];
+        if(Ruta.existe(idCliente,idCuenta)){
+            File rutaCuenta = new File(Ruta.path(idCliente,idCuenta));
+            try{
+                File cuenta = new File (rutaCuenta.getPath()+"/info.txt");
+                Scanner archivo = new Scanner(cuenta);
+                String info;
+                
+                while(archivo.hasNextLine()){
+                    info = archivo.nextLine();
+                    if(contador == 1){
+                        info = String.valueOf(saldo);
+                    }
+                    arreglo[contador] = info;
+                    contador++;
+                }
+                archivo.close();
 
-        
+                contador = 0;
+                FileWriter escribir = new FileWriter(cuenta,false);
+                while(archivo.hasNextLine()){
+                    escribir.write(arreglo[contador]);
+                    contador++;
+                }
+                escribir.close();
 
+            }catch (Exception e) {
+               System.out.println("Ocurrio un error");
+            }
+        }
     }
 
     public static String[] getTransacciones(String idCliente, String idCuenta){
@@ -87,9 +137,53 @@ public class Cuenta {
     }
 
     public static void retiro(String idCliente, String idCuenta, int monto){
+        int contador = 0;
+        int saldo = 0;
+        String arreglo [] = new String [4];
+        int saldo_nuevo = 0;
+        if(Ruta.existe(idCliente,idCuenta)){
+            File rutaCuenta = new File(Ruta.path(idCliente,idCuenta));
+            try{
+                File cuenta = new File (rutaCuenta.getPath()+"/info.txt");
+                try (Scanner archivo = new Scanner(cuenta)) {
+                    String info;
+                    
+                    while(archivo.hasNextLine()){
+                        info = archivo.nextLine();
+                        if(contador == 1){
+                            saldo = Integer.valueOf(info);
+                            if(saldo >= monto){
+                                saldo_nuevo = saldo - monto;
+                                info = String.valueOf(saldo_nuevo);
+                            }else{
+                                System.out.println("El monto a retirar debe ser menor o igual al saldo");
+                                return;
+                            }
+                        }
+                        arreglo[contador] = info;
+                        contador++;
+                    }
+                    archivo.close();
 
+                    contador = 0;
+                    FileWriter escribir = new FileWriter(cuenta,false);
+                    while(archivo.hasNextLine()){
+                        escribir.write(arreglo[contador]);
+                        contador++;
+                    }
+                    escribir.close();
+                }
+                Transaccion.guardarTransaccion(Transaccion.toString(SimpleDateFormat("yyyy/MM/dd").format(new Date()), "retiro", String.valueOf(monto), 
+                String.valueOf(Cuenta.getSaldo(idCliente,idCuenta)+monto), String.valueOf(Cuenta.getSaldo(idCliente,idCuenta))), idCliente,idCuenta);
+            }catch (Exception e) {
+               System.out.println("Ocurrio un error");
+            }
+        }
         
+    }
 
+    private static DateFormat SimpleDateFormat(String string) {
+        return null;
     }
 
     public static void deposito(String idCliente, String idCuenta, int monto){
