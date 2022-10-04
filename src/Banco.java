f409d95ec5cd7631010f8fd2a9c51c97240e2af0
Carlos May <carlosmay220@gmail.com>
2022-10-04 03:57:35 -0500

    Añadir cliente y banco
    uwu

--- src/Banco.java
+++ src/Banco.java
@@ -1,16 +1,35 @@
+import java.io.File;
 
 public class Banco {
 
-
     public static String[] getClientes(){
 
-        return {};
+        String [] clientes = new String [50];
+
+        File rutabanco = new File(Ruta.pathBanco());
+
+        String [] archivos = rutabanco.list();
+
+        for (int posicion=0; posicion<archivos.length; posicion++){
+
+            File directorio = new File (rutabanco, archivos[posicion]);
+
+            if (directorio.isDirectory()){
+                clientes[posicion] = archivos[posicion];
+            }
+        }
+        return clientes;
 
     }
 
     public static int getNumClientesTotales() {
         
-        return 1;    
+        File rutabanco = new File(Ruta.pathBanco());
+        String [] archivos = rutabanco.list();
+        int numClientesTotales = archivos.length-1;
+
+        return numClientesTotales;
     
     }
+
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,18 +1,59 @@
+import java.io.File;
+import java.util.Scanner;
+
+
 public class Cliente {
 
     public static String getNombre(String idCliente){
 
-        return "";
+        File rutaCliente = new File(Ruta.path(idCliente));
+        String[] cliente = new String [3];
+        int cont = 0;
+
+        try {
+            File cuenta = new File(rutaCliente+"info.txt");
+            try (Scanner archivo = new Scanner(cuenta)) {
+                while(archivo.hasNextLine()){
+                    cliente[cont] = archivo.nextLine();
+                    cont++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error al escribir");
+        }
+        
+        return cliente[2];
 
     }
     public static String[] getCuentas(String idCliente){
 
-        return {};
+        String [] cuentas = new String [50];
+
+        File rutaCliente = new File(Ruta.path(idCliente));
+
+        String [] archivos = rutaCliente.list();
+
+        for (int posicion=0; posicion<archivos.length; posicion++){
+
+            File directorio = new File (rutaCliente, archivos[posicion]);
+
+            if (directorio.isDirectory()){
+                cuentas[posicion] = archivos[posicion];
+            }
+        }
+
+        return cuentas;
 
     }
-    public static String getNumeroCuentas(String idCliente){
+    public static int getNumeroCuentas(String idCliente){
 
-        return "";
+
+        File rutaCuenta = new File(Ruta.path(idCliente));
+        String [] archivos = rutaCuenta.list();
+        int numCuentasTotales = archivos.length-1;
+
+        return numCuentasTotales;
 
     }
+
 }
